a858756b4ec35b130ec04488532fafaa3ca79cc8
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-28 17:08:17 +0300

    05

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,58 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import './styles.css';
+// import './styles.css';
 
-import { ToastContainer } from 'react-toastify'; // повідомлення
-import 'react-toastify/dist/ReactToastify.css'; // стилі повідомлень
+// import { ToastContainer } from 'react-toastify'; // повідомлення
+// import 'react-toastify/dist/ReactToastify.css'; // стилі повідомлень
 
-import { Searchbar } from './Searchbar/Searchbar';
-import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Searchbar } from './Searchbar/Searchbar';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
 
-// * Рефакторінг в Хуки
-export const App = () => {
-  const [request, setRequest] = useState('');
-
-  // Отримання даних запиту з форми
-  const onSubmit = requestValue => {
-    setRequest(requestValue);
-  };
-
-  return (
-    <>
-      {/* Форма пошуку: */}
-      <Searchbar onSubmit={onSubmit} />
-
-      {/* Галерея зображень */}
-      <ImageGallery request={request}></ImageGallery>
-
-      {/* Контейнер для повідомлень: */}
-      <ToastContainer newestOnTop={true} autoClose={4000} />
-    </>
-  );
-};
-
-// export class App extends Component {
-//   state = {
-//     request: '',
-//   };
+// // * Рефакторінг в Хуки
+// export const App = () => {
+//   const [request, setRequest] = useState('');
 
 //   // Отримання даних запиту з форми
-//   onSubmit = requestValue => {
-//     this.setState({ request: requestValue });
+//   const onSubmit = requestValue => {
+//     setRequest(requestValue);
 //   };
 
-//   render() {
-//     return (
-//       <>
-//         {/* Форма пошуку: */}
-//         <Searchbar onSubmit={this.onSubmit} />
+//   return (
+//     <>
+//       {/* Форма пошуку: */}
+//       <Searchbar onSubmit={onSubmit} />
 
-//         {/* Галерея зображень */}
-//         <ImageGallery request={this.state.request}></ImageGallery>
+//       {/* Галерея зображень */}
+//       <ImageGallery request={request}></ImageGallery>
 
-//         {/* Контейнер для повідомлень: */}
-//         <ToastContainer newestOnTop={true} autoClose={4000} />
-//       </>
-//     );
-//   }
-// }
+//       {/* Контейнер для повідомлень: */}
+//       <ToastContainer newestOnTop={true} autoClose={4000} />
+//     </>
+//   );
+// };
+
+// // export class App extends Component {
+// //   state = {
+// //     request: '',
+// //   };
+
+// //   // Отримання даних запиту з форми
+// //   onSubmit = requestValue => {
+// //     this.setState({ request: requestValue });
+// //   };
+
+// //   render() {
+// //     return (
+// //       <>
+// //         {/* Форма пошуку: */}
+// //         <Searchbar onSubmit={this.onSubmit} />
+
+// //         {/* Галерея зображень */}
+// //         <ImageGallery request={this.state.request}></ImageGallery>
+
+// //         {/* Контейнер для повідомлень: */}
+// //         <ToastContainer newestOnTop={true} autoClose={4000} />
+// //       </>
+// //     );
+// //   }
+// // }
