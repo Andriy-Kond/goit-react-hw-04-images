d7f61f15280af50ac7f780decfa1f81a688da094
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-28 17:30:45 +0300

    11

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './styles.css';
 
-import { ToastContainer } from 'react-toastify'; // повідомлення
-import 'react-toastify/dist/ReactToastify.css'; // стилі повідомлень
+// import { ToastContainer } from 'react-toastify'; // повідомлення
+// import 'react-toastify/dist/ReactToastify.css'; // стилі повідомлень
 
 // import { Searchbar } from './Searchbar/Searchbar';
 // import { ImageGallery } from './ImageGallery/ImageGallery';
 
 // * Рефакторінг в Хуки
 export const App = () => {
-  const [request, setRequest] = useState('');
+  // const [request, setRequest] = useState('');
 
-  // Отримання даних запиту з форми
-  const onSubmit = requestValue => {
-    setRequest(requestValue);
-  };
+  // // Отримання даних запиту з форми
+  // const onSubmit = requestValue => {
+  //   setRequest(requestValue);
+  // };
 
   return (
     <>
@@ -25,7 +25,7 @@ export const App = () => {
       {/* <ImageGallery request={request}></ImageGallery> */}
 
       {/* Контейнер для повідомлень: */}
-      <ToastContainer newestOnTop={true} autoClose={4000} />
+      {/* <ToastContainer newestOnTop={true} autoClose={4000} /> */}
     </>
   );
 };
